4204c67244dbed1bea4b0b022af4a91384b9e210
“milandudhat” <97076802+yash1001-sojitra@users.noreply.github.com>
2023-02-27 20:51:51 +0530

    fixes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,44 +6,111 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final style = const TextStyle(fontSize: 18);
     List item = ["Compact", "Compact Pro", "Premium", "Premium Plus"];
     List price = ["1599", "3599", "5599", "6599"];
+    List benifits = [
+      "4 days validity",
+      "Instagram marketing of your property",
+      "Top slot researved for better search",
+      "Rental agreement delivered to your home",
+      "Privacy of your contact number",
+      "Field assistant",
+      "Profestional photos of your property",
+      "Rentl represantataion on your behalf",
+      "Relationship manager"
+    ];
     return Scaffold(
       appBar: AppBar(),
       drawer: const Drawer(),
       body: SafeArea(
-          child: Column(
-        children: [
-          const Text("Get quickly"),
-          Card(
-            child: Column(
-              children: [
-                GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                  ),
-                  itemCount: 2,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Cardmodel(quility: item[index], price: price[index]);
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("+18% GST"),
-                    Text("T & C apply"),
-                  ],
-                )
-              ],
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                "• Get quickly",
+                style: style,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Card(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  side: BorderSide(color: Colors.grey, width: 0.5)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, childAspectRatio: 1.8),
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Cardmodel(
+                            quility: item[index], price: price[index]);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("+18% GST"),
+                          Text("T & C apply"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                "• Benifit Provided",
+                style: style,
+              ),
+            ),
+            const Divider(),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 1,
+                  crossAxisSpacing: 1,
+                  childAspectRatio: 3.5),
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                return BenifitModel(
+                  radiovalue: price[index],
+                  benifits: item[index],
+                  index: index.toString(),
+                  istrue: false,
+                );
+              },
+            ),
+          ],
+        ),
       )),
     );
   }
@@ -60,25 +128,48 @@ class Cardmodel extends StatefulWidget {
 class _CardmodelState extends State<Cardmodel> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(widget.quility),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Card(
+      color: Colors.grey[100],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('\u{20B9}${widget.price}'),
-            Radio(
-                value: widget.price,
-                groupValue: widget.price,
-                onChanged: ((value) {
-                  setState(() {
-                    widget.price == value;
-                  });
-                }))
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                widget.quility,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\u{20B9} ${widget.price}',
+                    style: const TextStyle(fontSize: 17),
+                  ),
+                  Radio(
+                      value: widget.price,
+                      groupValue: widget.price,
+                      onChanged: ((value) {
+                        setState(() {
+                          widget.price == value;
+                        });
+                      }))
+                ],
+              ),
+            )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
@@ -86,7 +177,14 @@ class _CardmodelState extends State<Cardmodel> {
 class BenifitModel extends StatefulWidget {
   String radiovalue;
   String benifits;
-  BenifitModel({required this.radiovalue, required this.benifits, super.key});
+  String index;
+  bool istrue;
+  BenifitModel(
+      {required this.radiovalue,
+      required this.benifits,
+      required this.index,
+      required this.istrue,
+      super.key});
 
   @override
   State<BenifitModel> createState() => _BenifitModelState();
@@ -97,15 +195,27 @@ class _BenifitModelState extends State<BenifitModel> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Radio(
-            value: widget.radiovalue,
-            groupValue: widget.radiovalue,
-            onChanged: (value) {
-              setState(() {
-                widget.radiovalue == value;
-              });
-            }),
-        SizedBox(
+        Container(
+            decoration: const BoxDecoration(
+              border: Border(right: BorderSide()),
+            ),
+            child: Text(
+              "${widget.index} ",
+              style: const TextStyle(fontSize: 15),
+            )),
+        const SizedBox(
+          width: 5,
+        ),
+        widget.istrue
+            ? Icon(
+                Icons.check,
+                color: Colors.green,
+              )
+            : Icon(
+                Icons.check,
+                color: Colors.white,
+              ),
+        const SizedBox(
           width: 5,
         ),
         Text(
